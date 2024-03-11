40f30348e81713d102247affb97514c81d7b2323
Felipe Sieiro <125573345+FelipeSieiro@users.noreply.github.com>
2024-03-11 19:42:29 -0300

    feat: initialize project with spring

---
+++ src/main/java/br/com/fiap/barberschedule/BarberscheduleApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.barberschedule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarberscheduleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarberscheduleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/fiap/barberschedule/BarberscheduleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.barberschedule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BarberscheduleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
