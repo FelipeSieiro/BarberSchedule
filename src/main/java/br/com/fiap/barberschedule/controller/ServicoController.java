ba07dd3e965230ab4223ebeac384635626613544
codelize <lflizier@gmail.com>
2024-03-12 21:00:58 -0300

    feat: add delete requests

--- src/main/java/br/com/fiap/barberschedule/controller/ServicoController.java
+++ src/main/java/br/com/fiap/barberschedule/controller/ServicoController.java
@@ -1,11 +1,16 @@
 package br.com.fiap.barberschedule.controller;
 
 import java.util.List;
+import java.util.Optional;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,6 +42,19 @@ public class ServicoController {
         repository.save(servico);
         return servico;
     }
+    
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        Optional<Servico> optionalServico = repository.findById(id);
+        if (optionalServico.isPresent()) {
+            repository.deleteById(id);
+            log.info("Serviço removido com sucesso: ID {}", id);
+            return ResponseEntity.noContent().build();
+        } else { 
+            log.error("Serviço não encontrado para o ID: {}", id);
+            return ResponseEntity.notFound().build();
+        }
+    } 
 }
 
      
