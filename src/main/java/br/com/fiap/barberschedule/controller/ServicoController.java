1e9119d1fcc3fff4f08b244700eb1779c5f42f03
FelipeSieiro <felipedossantos0804@gmail.com>
2024-03-18 18:34:21 -0300

    refactor: remove http requests

--- src/main/java/br/com/fiap/barberschedule/controller/ServicoController.java
+++ src/main/java/br/com/fiap/barberschedule/controller/ServicoController.java
@@ -1,10 +1,7 @@
 package br.com.fiap.barberschedule.controller;
 
 import java.util.List;
-import java.util.Optional;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,23 +9,27 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
 
 import br.com.fiap.barberschedule.model.Servico;
 import br.com.fiap.barberschedule.repository.ServicoRepository;
+import lombok.extern.slf4j.Slf4j;
 
 
 @RestController
 @RequestMapping("servico")
+@Slf4j
+@SuppressWarnings("null")
 public class ServicoController {
-
-      Logger log = LoggerFactory.getLogger(getClass());
 
     @Autowired 
     ServicoRepository repository;
+    
 
     @GetMapping
     public List<Servico> index() {
@@ -42,19 +43,7 @@ public class ServicoController {
         repository.save(servico);
         return servico;
     }
-    
-    @DeleteMapping("/{id}")
-    public ResponseEntity<Void> delete(@PathVariable Long id) {
-        Optional<Servico> optionalServico = repository.findById(id);
-        if (optionalServico.isPresent()) {
-            repository.deleteById(id);
-            log.info("Serviço removido com sucesso: ID {}", id);
-            return ResponseEntity.noContent().build();
-        } else { 
-            log.error("Serviço não encontrado para o ID: {}", id);
-            return ResponseEntity.notFound().build();
-        }
-    } 
+
 }
 
      
